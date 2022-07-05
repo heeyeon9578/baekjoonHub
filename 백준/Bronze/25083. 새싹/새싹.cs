96e2ef02ce5db07183d291f410bc4b8fa306ec19
heeyeon <48540554+heeyeon9578@users.noreply.github.com>
2022-07-05 10:05:49 +0900

    [Bronze V] Title: 새싹, Time: 52 ms, Memory: 5300 KB -BaekjoonHub

---
+++ 백준/Bronze/25083. 새싹/새싹.cs
@@ -0,0 +1,17 @@
+using System;
+public class Program
+{
+
+    public static void Main()
+    {
+
+        Console.WriteLine("         ,r\'\"7");
+        Console.WriteLine("r`-_   ,\'  ,/");
+        Console.WriteLine(" \\. \". L_r\'");
+        Console.WriteLine("   `~\\/");
+        Console.WriteLine("      |");
+        Console.WriteLine("      |");
+
+
+    }
+}
