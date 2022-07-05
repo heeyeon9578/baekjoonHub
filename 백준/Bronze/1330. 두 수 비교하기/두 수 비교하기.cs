a44cb6e931866ed30ce15adbeedf5a08f0093c65
heeyeon <48540554+heeyeon9578@users.noreply.github.com>
2022-07-05 10:13:17 +0900

    [Bronze V] Title: 두 수 비교하기, Time: 60 ms, Memory: 5472 KB -BaekjoonHub

--- 백준/Bronze/1330. 두 수 비교하기/두 수 비교하기.cs
+++ 백준/Bronze/1330. 두 수 비교하기/두 수 비교하기.cs
@@ -4,15 +4,22 @@ public class Program
 
     public static void Main()
     {
-        //입력 첫째줄에 정수 A,B를 입력받는다.
-        //정수 A,B는 공백(스페이스)로 구분되어진다.
-        string[] a = Console.ReadLine().Split(' ');
-        int numA = int.Parse(a[0]);
-        int numB = int.Parse(a[1]);
 
-        //출력 첫째줄에 값을 비교하여 '>', '<', '==' 비교연산자를 출력한다.
-        if (numA > numB) Console.WriteLine(">");
-        else if (numA < numB) Console.WriteLine("<");
-        else if (numA == numB) Console.WriteLine("==");
+        String[] inputs = Console.ReadLine().Split(' ');
+        int a = Convert.ToInt32(inputs[0]);
+        int b = Convert.ToInt32(inputs[1]);
+
+        if (a > b)
+        {
+            Console.WriteLine(">");
+        }else if(a < b)
+        {
+            Console.WriteLine('<');
+        }
+        else if( a==b)
+        {
+            Console.WriteLine("==");
+        }
+
     }
 }
