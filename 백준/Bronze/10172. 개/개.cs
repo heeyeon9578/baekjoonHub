0340bbf8c95c2aa579fea5fa94a7d022db053e20
heeyeon <48540554+heeyeon9578@users.noreply.github.com>
2022-07-05 09:06:46 +0900

    [Bronze V] Title: 개, Time: 48 ms, Memory: 5308 KB -BaekjoonHub

---
+++ 백준/Bronze/10172. 개/개.cs
@@ -0,0 +1,14 @@
+using System;
+public class Program
+{
+
+    public static void Main()
+    {
+        Console.WriteLine("|\\_/|");
+        Console.WriteLine("|q p|   /}");
+        Console.WriteLine("( 0 )\"\"\"\\");
+        Console.WriteLine("|\"^\"`    |");
+        Console.WriteLine("||_/=\\\\__|");
+
+    }
+}
