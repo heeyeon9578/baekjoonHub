241f2bc73c0b0d2992084ec5298ee9cba5e5c19c
heeyeon <48540554+heeyeon9578@users.noreply.github.com>
2022-07-05 09:40:02 +0900

    [Bronze V] Title: ??!, Time: 60 ms, Memory: 5352 KB -BaekjoonHub

---
+++ 백준/Bronze/10926. ？？！/？？！.cs
@@ -0,0 +1,12 @@
+using System;
+public class Program
+{
+
+    public static void Main()
+    {
+        String inputs = Console.ReadLine();
+        Console.WriteLine(inputs + "??!");
+
+
+    }
+}
