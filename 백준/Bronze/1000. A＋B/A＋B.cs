ce113cb171070088089ff2bf871eb06d56c9e42d
heeyeon <48540554+heeyeon9578@users.noreply.github.com>
2022-07-05 09:14:10 +0900

    [Bronze V] Title: A+B, Time: 64 ms, Memory: 5500 KB -BaekjoonHub

---
+++ 백준/Bronze/1000. A＋B/A＋B.cs
@@ -0,0 +1,16 @@
+using System;
+public class Program
+{
+
+    public static void Main()
+    {
+        String input = Console.ReadLine();
+        String[] inputs = input.Split(' ');
+        int ch1 = Convert.ToInt32(inputs[0]);
+        int ch2 = Convert.ToInt32(inputs[1]);
+
+        Console.WriteLine(ch1+ ch2);
+
+
+    }
+}
