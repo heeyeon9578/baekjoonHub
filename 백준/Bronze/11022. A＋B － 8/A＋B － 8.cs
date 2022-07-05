44389cb7fd70cdf8d8739ea61c3d9cad24cf24a9
heeyeon <48540554+heeyeon9578@users.noreply.github.com>
2022-07-05 14:54:08 +0900

    [Bronze III] Title: A+B - 8, Time: 76 ms, Memory: 5992 KB -BaekjoonHub

---
+++ 백준/Bronze/11022. A＋B － 8/A＋B － 8.cs
@@ -0,0 +1,29 @@
+using System;
+using System.Text;
+
+public class Program
+{
+
+    public static void Main()
+    {
+        StringBuilder sb = new StringBuilder(); 
+        int N = int.Parse(Console.ReadLine());
+        int[] a = new int[N];
+        int[] b = new int[N];
+
+        for(int i = 0; i < N; i++)
+        {
+            String[] inputs = Console.ReadLine().Split(' ');
+            a[i] = int.Parse(inputs[0]);
+            b[i] = int.Parse(inputs[1]);
+
+            sb.Append("Case #"+(i+1)+": "+a[i] +" + "+ b[i] +" = " +(a[i] + b[i])+"\n");
+               
+            
+        }
+
+        Console.WriteLine(sb.ToString());
+
+    }
+
+}
