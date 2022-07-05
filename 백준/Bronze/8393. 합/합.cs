7dab37a7791c10b12cffc05ff3b5b83cccce0b30
heeyeon <48540554+heeyeon9578@users.noreply.github.com>
2022-07-05 12:19:54 +0900

    [Bronze III] Title: 합, Time: 60 ms, Memory: 5480 KB -BaekjoonHub

---
+++ 백준/Bronze/8393. 합/합.cs
@@ -0,0 +1,18 @@
+using System;
+public class Program
+{
+
+    public static void Main()
+    {
+        int n = int.Parse(Console.ReadLine());
+        int sum = 0;
+        for(int i=1; i<n+1; i++)
+        {
+            sum += i;
+        }
+
+        Console.WriteLine(sum); 
+        
+    }
+
+}
