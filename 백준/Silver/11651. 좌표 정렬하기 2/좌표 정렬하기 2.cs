8542b360d22a0913d406793e1d74b23dd856d8dc
heeyeon <48540554+heeyeon9578@users.noreply.github.com>
2022-08-05 11:50:30 +0900

    [Silver V] Title: 좌표 정렬하기 2, Time: 256 ms, Memory: 23100 KB -BaekjoonHub

---
+++ 백준/Silver/11651. 좌표 정렬하기 2/좌표 정렬하기 2.cs
@@ -0,0 +1,48 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+//
+//
+//
+
+namespace Project2
+{
+    class Class1
+    {
+        static void Main(string[] args)
+        {
+
+            //[1]Input
+            StreamWriter writer = new StreamWriter(Console.OpenStandardOutput());
+            StreamReader reader = new StreamReader(Console.OpenStandardInput());
+
+            int n = int.Parse(reader.ReadLine());
+            List<(int, int)> list = new List<(int, int)>();
+            //[2]process
+            for (int i=0; i<n; i++)
+            {
+                string[] str = reader.ReadLine().Split();
+                int x = int.Parse(str[0]);
+                int y = int.Parse(str[1]);
+                list.Add((x, y));
+            }
+
+            var sortList = list.OrderBy(x => x.Item2).ThenBy(x => x.Item1).ToList();
+            //[3]Output
+            for (int i=0; i<n; i++)
+            {
+                writer.WriteLine($"{sortList[i].Item1} {sortList[i].Item2}");
+            }
+
+            writer.Close();
+            reader.Close();
+
+
+        }
+        
+    }
+}
+
