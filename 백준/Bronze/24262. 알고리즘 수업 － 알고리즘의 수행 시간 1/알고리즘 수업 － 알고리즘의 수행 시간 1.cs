8d7548266307e967f4f5730d3d997d14ed8b6054
heeyeon <48540554+heeyeon9578@users.noreply.github.com>
2023-03-22 14:05:05 +0900

    [Bronze V] Title: 알고리즘 수업 - 알고리즘의 수행 시간 1, Time: 64 ms, Memory: 5484 KB -BaekjoonHub

---
+++ 백준/Bronze/24262. 알고리즘 수업 － 알고리즘의 수행 시간 1/알고리즘 수업 － 알고리즘의 수행 시간 1.cs
@@ -0,0 +1,21 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project1
+{
+    internal class Class1
+    {
+        static void Main(string[] args)
+        {
+            string inputValue = Console.ReadLine();
+            int inputValueNum= int.Parse(inputValue);
+            Console.WriteLine(1);   
+            Console.WriteLine(0);
+
+
+        }
+    }
+}
